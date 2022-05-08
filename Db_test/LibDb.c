604476e524edaa6101f838ff61a2d7db671e4725
D3vast4tor <d3vast4tor@protonmail.com>
2022-05-08 22:45:57 +0200

    .

--- Db_test/LibDb.c
+++ Db_test/LibDb.c
@@ -8,10 +8,6 @@
 
 
 */
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 struct User{
     char username[20];
     char password[20];
@@ -32,8 +28,8 @@ struct Node * __init__(struct Node * head,char * username,char * password){
 }
 
 struct Node * add_user(struct Node * head,char * username,char * password){
-    struct Node * cur,new_node;
-    new_node = malloc(sizeof(struct Node));
+    struct Node *cur,*new_node;
+    new_node = (struct Node*)malloc(sizeof(struct Node));
     strcpy(new_node->data->username,username);
     strcpy(new_node->data->password,password);
     new_node->next == NULL;
@@ -47,33 +43,42 @@ struct Node * add_user(struct Node * head,char * username,char * password){
 }
 
 struct Node * rm_user(struct Node * head,char * username){
-    struct Node * prec,cur,succ;
+    struct Node *prec,*cur,*succ;
     for(cur = head;;cur = cur->next){
         if(cur->data->username != username){
             prec = cur;
         }else if(cur->data->username == username){
             succ = cur->next;
             prec->next = succ;
-            cur = NULL;
             break;
         }
     }
     return head;
 }
+void list_user(void){
+    struct Node * cur;
+    int i = 0;
+    for(cur = head;cur->next != NULL;cur = cur->next){
+        printf("\nUser n°%d\nUsername: %s",++i,cur->data->username);
+
+    }
+}
 int main(){
-    char username[20],password[20]conf_passwrd[20];
+    char username[20],password[20],conf_passwrd[20];
     char choise;
     do{
+        printf("\n*************\nADMIN PANEL\n*************\na)Add user.\nr)Remove user.\nl)List Database.\nq)Quit.\nType your choise: ");
+        scanf(" %c",&choise);
         switch(choise){
             case 'a':
                 printf("\nType the username: ");
-                scanf(" %s",%username);
+                scanf(" %s",&username);
                 printf("\nType the password: ");
-                scanf(" %s",%password);
+                scanf(" %s",&password);
                 printf("\nRetype the password: ");
                 scanf(" %s",&conf_passwrd);
-                if(password == conf_passwrd){
-                    if(head == null){
+                if(strcpm(password,conf_passwrd) == 0){
+                    if(head == NULL){
                         head = __init__(head,username,password);
                     }else{
                         head = add_user(head,username,password);
@@ -83,14 +88,11 @@ int main(){
             case 'r':
             
             break;
-            case 'q';
+            case 'l':
+                list_user();
+            case 'q':
 
             break;
         }
-    }while(choise != 'q')
-    
-    
-
-    head = 
-    
+    }while(choise != 'q');
 }
